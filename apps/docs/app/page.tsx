8f6ae8f387c41b693cb422e409cbf1c9c3bc59e4
r.sohrabi <rezasohrabi1995@yahoo.com>
2025-10-24 17:31:27 +0330

    feat(react-iran-maps): update Map component to use comprehensive mock province data and simplify color scaling; adjust global styles for improved contrast

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -2,6 +2,7 @@
 
 import styles from "./page.module.css";
 import { Map } from "react-iran-maps";
+import { mockProvinceData } from "./mockProvinceData";
 
 export default function Home() {
   return (
@@ -18,43 +19,7 @@ export default function Home() {
             width={800}
             height={600}
             aspectRatio="1.23"
-            data={[
-              {
-                name: "Tehran",
-                count: 100,
-                counties: [
-                  { name: "Tehran", count: 100 },
-                  { name: "Shiraz", count: 200 },
-                  { name: "Tabriz", count: 300 },
-                  { name: "Yazd", count: 400 },
-                  { name: "Esfahan", count: 500 },
-                  { name: "Mashhad", count: 600 },
-                  { name: "Qom", count: 700 },
-                  { name: "Kerman", count: 800 },
-                ],
-              },
-              {
-                name: "Isfahan",
-                count: 200,
-                counties: [
-                  { name: "Isfahan", count: 200 },
-                  { name: "Shiraz", count: 300 },
-                  { name: "Tabriz", count: 400 },
-                  { name: "Yazd", count: 500 },
-                  { name: "Esfahan", count: 600 },
-                  { name: "Mashhad", count: 700 },
-                  { name: "Qom", count: 800 },
-                  { name: "Kerman", count: 900 },
-                ],
-              },
-              { name: "Shiraz", count: 300 },
-              { name: "Tabriz", count: 400 },
-              { name: "Yazd", count: 500 },
-              { name: "Esfahan", count: 600 },
-              { name: "Mashhad", count: 700 },
-              { name: "Qom", count: 800 },
-              { name: "Kerman", count: 900 },
-            ]}
+            data={mockProvinceData}
             colorScale={["#E0F2FE", "#1E3A8A"]}
           />
         </div>
